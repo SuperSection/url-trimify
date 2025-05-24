e3336edc6cc3c44dd00d517e9fade7c1fe5041e4
SuperSection <soumosarkar297@gmail.com>
2025-05-25 04:28:25 +0530

    Setup SpringBoot backend for the project

---
+++ url-trimify-backend/src/main/java/com/supersection/trimify/UrlTrimifyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.supersection.trimify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlTrimifyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlTrimifyBackendApplication.class, args);
+	}
+
+}
---
+++ url-trimify-backend/src/test/java/com/supersection/trimify/UrlTrimifyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supersection.trimify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlTrimifyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
